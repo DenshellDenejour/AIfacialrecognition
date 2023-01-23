ab2fdfba99051fe8abb42c4cf3f3e06d8877abe4
Denshell Denejour <121324587+DenshellDenejour@users.noreply.github.com>
2023-01-23 19:37:23 -0400

    Create AI.js
    this is my try of a facial recognition with js

---
+++ AI.js
@@ -0,0 +1,26 @@
+<div class="container">
+  <div id= "face">
+  </div>
+   
+  <video id="video" width="1280" heigth="720" autoplay>
+
+</div>
+
+ <div>
+    <label for="deviceSelector">
+      Select device
+    </label>
+   <select id="deviceSelector">
+   </select>
+ </div>
+ 
+ <div>
+    <label for="faceSelector">
+        Select face
+      </label>
+     <select id="faceSelector"></select>
+ </div>
+ 
+ <button id="start">
+    start video
+    </button>
